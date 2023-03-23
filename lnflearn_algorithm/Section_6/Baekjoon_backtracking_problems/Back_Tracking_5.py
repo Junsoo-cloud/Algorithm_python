a18cb62396bac8bcf2bf6301f311e5e4c8efd5f2
Junsoo-cloud <kfxsmt1@naver.com>
2023-03-23 14:53:19 +0000

    inflearn_algorithms

---
+++ lnflearn_algorithm/Section_6/Baekjoon_backtracking_problems/Back_Tracking_5.py
@@ -0,0 +1,19 @@
+'''N-Queen'''
+import sys
+sys.stdin=open('input.txt', 'r')
+def adjacent(x)
+
+
+def DFS(v):
+    if v == n:
+        return
+    else:
+        for i in range(1, n+1):
+            
+
+
+
+if __name__ == "__main__":
+    n = int(input())
+    matrix = [[0]*n for _ in range(n)]
+    DFS(1)
