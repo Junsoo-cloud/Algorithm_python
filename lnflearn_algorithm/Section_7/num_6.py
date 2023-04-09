3f54cceaf6e870aba4858c92821b0481af28c0ba
Junsoo-cloud <kfxsmt1@naver.com>
2023-04-09 14:48:12 +0000

    inflearn_algorithms

--- lnflearn_algorithm/Section_7/num_6.py
+++ lnflearn_algorithm/Section_7/num_6.py
@@ -1,22 +1,30 @@
-'''Num_6: 알파코드(DFS)'''
+'''Num_6: 알파코드 (DFS)'''
 import sys
 sys.stdin=open('input.txt', 'r')
 def DFS(L, P):
-    global N
-    print(N)
-    if L == len(N):
-        print(res)
+    global cnt
+    if L == tmp:
+        cnt += 1
+        for i in range(P): # P까지 도는것 -> P까지 res에 들어있기때문 !!
+            print(chr(res[i]+64), end = '')
+        print()
         return
     else:
         for i in range(1, 27):
-            if N[L] == str(i):
-                if len(str(i)) == 1:
-                    res[P] == i
-                    DFS(L+1, P+1)
-                if len(str(i)) == 2 and i <= 26:
-                    res[P] == i
-                    DFS(L+2, P+1)
+            if n[L] == i:
+                res[P] = i
+                DFS(L+1, P+1)
+            elif i>=10 and n[L] == i//10 and n[L+1] == i%10:
+                res[P] = i
+                DFS(L+2, P+1)
 if __name__ == "__main__":
-    N = str(input())
-    res = [0]*(len(N)+3)
+    n = list(map(int, input()))
+    tmp = len(n)
+    res = [0]*tmp
+    n.insert(tmp, -1) # indexError 방지
+    cnt = 0
     DFS(0, 0)
+    print(cnt)
+    
+# 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
+# 프로그래머스 파이썬 정규식
